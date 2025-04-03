6785ea09ac605a294bde608fb8a0be7ce6519e7f
Daniel <danielatahanov053@gmail.com>
2025-04-03 13:30:15 +0500

    commit 6

--- slider.js
+++ slider.js
@@ -1,24 +1,54 @@
+const slides = document.querySelectorAll('.slider');
 const dots = document.querySelectorAll('.dot');
-const dotsActive = document.querySelectorAll('.dot.active');
+const prevButton = document.getElementById('prevSlide');
+const nextButton = document.getElementById('nextSlide');
+let currentSlide = 0;
 
 dots.forEach((element) => {
   element.addEventListener('click', () => {
-    const dotId = element.getAttribute('data-sldie-dot');
-    return showSlide(dotId),dotsViseble(dotId);
+    const dotId = (element.getAttribute('data-slide-dot'));
+    showSlide(dotId);
+    dotsViseble(dotId);
   });
 });
 
 function showSlide(dotId) {
   document.querySelector('.slider.visible').classList.remove('visible');
-  document
-    .querySelector('.slider[data-slide="' + dotId + '"]')
-    .classList.add('visible');
-}
+  document.querySelector('.slider[data-slide="' + dotId + '"]').classList.add('visible');
 
+  prevButton.classList.toggle('disabled', dotId === 0);
+  nextButton.classList.toggle('disabled', dotId === slides.length - 1);
+
+  currentSlide = dotId;
+}
 
 function dotsViseble(dotId) {
-    document.querySelector('.dot.active').classList.remove('active');
-    document
-    .querySelector('.slider[data-slide="' + dotId + '"]')
-    .classList.add('visible');
+  document.querySelector('.dot.active').classList.remove('active');
+  document.querySelector('.dot[data-slide-dot="' + dotId + '"]').classList.add('active');
 }
+
+nextButton.addEventListener('click', () => {
+  if (currentSlide < slides.length - 1) {
+    showSlide(currentSlide + 1);
+    dotsViseble(currentSlide);
+  }
+
+});
+
+prevButton.addEventListener('click', () => {
+  if (currentSlide > 0) {
+    showSlide(currentSlide - 1);
+    dotsViseble(currentSlide);
+  }
+});
+
+
+showSlide(currentSlide);
+
+setInterval(() => {
+  if (currentSlide < slides.length - 1) {
+    currentSlide++;  
+    showSlide(currentSlide);  
+    dotsViseble(currentSlide);  
+  }
+}, 3000);
